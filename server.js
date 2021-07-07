71818345907cbfbec7f9637d564e639cf4a491d0
Biznaz <aldhelm7@gmail.com>
2021-07-07 13:00:22 -0700

    added create note function

--- server.js
+++ server.js
@@ -1,9 +1,34 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3002;
 const app = express();
 
+// Parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// Parse incoming JSON data
+app.use(express.json());
+
+function createNewNote(body, noteArray) {
+  const note = body;
+  noteArray.psuh(note);
+  fs.writeFileSync(
+    path.join(__dirname, './db/notes.json'),
+    JSON.stringify({ notes: noteArray }, null, 2)
+  );
+  return note;
+}
+
 app.get('/api/notes', (req, res) => {
   res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = notes.length.toString();
+
+  const note = createNewNote(req.body, notes);
+
+  res.json(note);
 });
 
 app.listen(PORT, () => {
