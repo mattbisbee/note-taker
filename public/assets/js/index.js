b3910c823178e2f4119ca530774888a94d4d71c0
Biznaz <aldhelm7@gmail.com>
2021-07-07 13:59:57 -0700

    connected front end with back end

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -75,6 +75,25 @@ const handleNoteSave = () => {
     getAndRenderNotes();
     renderActiveNote();
   });
+
+  fetch('/api/notes', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(animalObject)
+  })
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      alert('Error: ' + response.statusText);
+    })
+    .then(postResponse => {
+      console.log(postResponse);
+      alert('New note created!');
+    });
 };
 
 // Delete the clicked note
--- server.js
+++ server.js
@@ -4,14 +4,18 @@ const express = require('express');
 const PORT = process.env.PORT || 3002;
 const app = express();
 
+// Middleware
+//Makes these files available 
+app.use(express.static('public'));
 // Parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
 // Parse incoming JSON data
 app.use(express.json());
 
+// Create a new note
 function createNewNote(body, noteArray) {
   const note = body;
-  noteArray.psuh(note);
+  noteArray.push(note);
   fs.writeFileSync(
     path.join(__dirname, './db/notes.json'),
     JSON.stringify({ notes: noteArray }, null, 2)
@@ -21,6 +25,19 @@ function createNewNote(body, noteArray) {
 
 app.get('/api/notes', (req, res) => {
   res.json(notes);
+});
+
+//To get and display the html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+//To get and display the other html file
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+//In case there are no route matches, this should come last if more routes are added later!
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.post('/api/notes', (req, res) => {
