8a9ecef215f2680aba83b986687a18ca1016d668
Biznaz <aldhelm7@gmail.com>
2021-07-14 11:34:59 -0700

    last minute check on code

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -75,25 +75,6 @@ const handleNoteSave = () => {
     getAndRenderNotes();
     renderActiveNote();
   });
-
-  fetch('/api/notes', {
-    method: 'POST',
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(animalObject)
-  })
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      alert('Error: ' + response.statusText);
-    })
-    .then(postResponse => {
-      console.log(postResponse);
-      alert('New note created!');
-    });
 };
 
 // Delete the clicked note
